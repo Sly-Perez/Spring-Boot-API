135cd4195e58737a7808cb246b8852db38a3aaef
Sly-Perez <elaipereza@gmail.com>
2025-02-12 13:36:16 -0500

    Load of main class of application

---
+++ src/main/java/com/proFightingFicAcademy/ProFightingFicAcademyApplication.java
@@ -0,0 +1,13 @@
+package com.proFightingFicAcademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProFightingFicAcademyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProFightingFicAcademyApplication.class, args);
+	}
+
+}
