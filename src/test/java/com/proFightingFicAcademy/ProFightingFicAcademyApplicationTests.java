f8350ffee4ded35fa3ffc42c6a2d0f5bb65d8d23
Sly-Perez <elaipereza@gmail.com>
2025-02-12 13:34:39 -0500

    Load of Test folder

---
+++ src/test/java/com/proFightingFicAcademy/ProFightingFicAcademyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proFightingFicAcademy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProFightingFicAcademyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
